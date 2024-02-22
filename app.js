e19bf97b7a1f88164b9a282ec8d307e53b3d590d
Zaid Mohammed <zaid.md221@gmail.com>
2024-02-23 01:33:54 +0530

    Update Documentation

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(cors());
 app.use(xss());
 
 app.get("/", (req, res) => {
-  res.send(`<h1>JOBS API</h1><a href"/api-docs"> Documentation</a> `);
+  res.send(`<h1>JOBS API</h1><a href="/api-docs"> Documentation</a> `);
 });
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
